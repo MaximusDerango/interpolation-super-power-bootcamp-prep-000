ab06bf5c5cbf4a14600a31a4f8efeb492f2a268a
Maxim Dalton <maximcdalton@gmail.com>
2018-06-01 20:25:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  
   colors.length.times do |i|
     color = colors[i]
     print "#{color[0].upcase}: #{color}#{i != colors.length-1 ? ", " : "\n"}"
