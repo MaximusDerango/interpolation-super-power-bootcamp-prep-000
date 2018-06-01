2beea481039438a08098962c780cb5f8595965fb
Maxim Dalton <maximcdalton@gmail.com>
2018-06-01 20:21:10 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,4 @@
 # Write your #display_rainbow method here
-# def display_rainbow(colors)
-#   formatted = colors.collect{|c| "#{c[0].upcase}: #{c}"}.join(", ")
-#   puts formatted
-#   formatted
-# end
-
 def display_rainbow(colors)
   colors.each_with_index do |x, i|
     print "#{x[0].upcase}: #{x}#{i != colors.length-1 ? ", " : "\n"}"
