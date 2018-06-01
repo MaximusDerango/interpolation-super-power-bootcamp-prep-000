bba18aec7c5b363fc627fbeb001e452fff4365b3
Maxim Dalton <maximcdalton@gmail.com>
2018-06-01 20:11:57 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors)
-  formatted = colors.collect{|c| "#{c[0].upcase}: #{c}"}.join(", ")
-  puts formatted
-  formatted
-end
+# def display_rainbow(colors)
+#   formatted = colors.collect{|c| "#{c[0].upcase}: #{c}"}.join(", ")
+#   puts formatted
+#   formatted
+# end
