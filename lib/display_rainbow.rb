5f90d9cf29ce8e3039a6738cd37b51325a9fbe59
Maxim Dalton <maximcdalton@gmail.com>
2018-06-01 20:18:57 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,6 +7,6 @@
 
 def display_rainbow(colors)
   colors.each_with_index do |x, i|
-    print "#{x[0].upcase}: #{x}#{"" if }"
+    print "#{x[0].upcase}: #{x}#{i != colors.length-1 ? ", " : ""}"
   end
 end
